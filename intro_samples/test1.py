8120680598e41343f1467e0a6ebe7d76134bd9a8
hugh <he.x.working@gmail.com>
2020-03-07 16:04:10 +1300

    input_data coded

--- intro_samples/test1.py
+++ intro_samples/test1.py
@@ -1,10 +1,19 @@
 import os
 import nni
 
+from intro_samples import input_data
+
 
 def main():
     pass
 
 
 if __name__ == '__main__':
+    params = {
+        'data_dir': './sample_data/input_data', 'dropout_rate': 0.5, 'channel_1_num': 32,
+        'channel_2_num': 64,
+        'conv_size': 5, 'pool_size': 2, 'hidden_size': 1024, 'learning_rate': 1e-4, 'batch_num': 2000,
+        'batch_size': 32
+    }
+    input_data.read_data_sets('./input_data', one_hot=True)
     main()
