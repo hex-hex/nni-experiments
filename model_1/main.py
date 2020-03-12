3f83709d27c2367d582d5cb3fdfb8d8e18e116f0
hugh <he.x.working@gmail.com>
2020-03-13 01:54:07 +1300

    make run

--- model_1/main.py
+++ model_1/main.py
@@ -1,3 +1,4 @@
+from tensorflow import keras
 from keras.initializers import RandomUniform, he_uniform
 from keras.regularizers import l2
 from sklearn.model_selection import train_test_split
@@ -20,4 +21,25 @@ if __name__ == '__main__':
     relu_init = he_uniform(seed=random_seed)
     embeddings_regu = l2(1e-6)
 
-    print(val)
+    n_users, n_movies = len(data_set.user_id.unique()), len(data_set.item_id.unique())
+    n_latent_factors = 16
+    a = keras.layers.Input(shape=(1,))
+    b = keras.layers.Dense(32)(a)
+    model = keras.Model(inputs=a, outputs=b)
+    movie_input = keras.layers.Input(shape=[1], name='Item')
+    movie_embedding = keras.layers.Embedding(n_movies, n_latent_factors,
+                                             embeddings_initializer=embedding_init,
+                                             embeddings_regularizer=embeddings_regu,
+                                             name='Movie-Embedding')(movie_input)
+    movie_vec = keras.layers.Flatten(name='FlattenMovies')(movie_embedding)
+
+    user_input = keras.layers.Input(shape=[1], name='User')
+    user_embedding = keras.layers.Embedding(n_users, n_latent_factors,
+                                            embeddings_initializer=embedding_init,
+                                            embeddings_regularizer=embeddings_regu,
+                                            name='User-Embedding')(user_input)
+    user_vec = keras.layers.Flatten(name='FlattenUsers')(user_embedding)
+    prod = keras.layers.dot([movie_vec, user_vec], axes=1, normalize=True, name='DotProduct')
+    model = keras.Model([user_input, movie_input], prod)
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
+    history = model.fit([train.user_id, train.item_id], train.rating, epochs=5, verbose=2)
